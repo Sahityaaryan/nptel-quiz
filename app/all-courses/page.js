fd935b2ddea0b1bc8dda7a16ff1e6866763bd222
sahityaaryan <sahityaaryansingh@gmail.com>
2025-04-20 21:26:51 +0530

    fix: fixed the reference error at the deployement of the 'redirect'

--- app/all-courses/page.js
+++ app/all-courses/page.js
@@ -1,19 +1,16 @@
 import AllCourses from "@/components/courses/AllCourses";
 import { fetchAllCourses } from "../lib/actions";
 import { getUserSession } from "../lib/session";
+import { redirect } from "next/navigation";
 
 export default async function Page() {
-    // const session = await getSession();
-    const user = await getUserSession();
-    if (!user) redirect('/login');
-  
-    const courses = await fetchAllCourses();
+  // const session = await getSession();
+  const user = await getUserSession();
+  if (!user) redirect("/login");
 
-    // console.log("subscribedCourses: ", subscribedCourses);
-    
-   
-    return (
-      <AllCourses courses={courses}/>
-    );
-  }
+  const courses = await fetchAllCourses();
 
+  // console.log("subscribedCourses: ", subscribedCourses);
+
+  return <AllCourses courses={courses} />;
+}
