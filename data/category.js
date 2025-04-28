528a889e2ee4534a8270b118751d7d8d93d1f7d5
sahityaaryan <sahityaaryansingh@gmail.com>
2025-04-29 01:22:45 +0530

    add: added category

---
+++ data/category.js
@@ -0,0 +1,14 @@
+export const courseCategory = [
+  {
+    title: "Programming",
+    value: "programming",
+  },
+  {
+    title: "Computer Science",
+    value: "computer_science",
+  },
+  {
+    title: "Electronics and Communication",
+    value: "electronics_and_communication",
+  },
+];
